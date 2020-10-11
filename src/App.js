fbbeb5de4f4b9a9175a39e611992026ecd4f3b7c
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-12 00:13:53 +0600

    finished pagination  component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Header from './Components/Header/Header';
 import { newsCategory } from './Components/NewsCategory/NewsCategory';
 import { NewsList } from './Components/NewsList/NewsList';
+import Pagination from './Components/Pagination/Pagination';
 
 const fakeNews = [
   {
@@ -37,7 +38,12 @@ class App extends Component {
         <div className="row">
           <div className="col-sm-6 offset-md-3">
             <Header category = {newsCategory.technology}/>
+            <div className="d-flex">
+              <p className="text-black-50">about {0} result found</p>
+              <p className="text-black-50 ml-auto">{1} page of {100} </p>
+            </div>
             <NewsList news={ fakeNews }/>
+            <Pagination/>
           </div>
         </div>
       </div>
--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -8,6 +8,23 @@ class Pagination extends Component {
         return (
             <div className="d-flex align-items-center my-5">
                 <button className="btn btn-warning">Previous</button>
+                <div className="flex-grow-1 text-center">
+                {this.state.isEditabele ? (
+                    <input type="number" value="1" name="" id=""/>
+                ):(
+                    <p style={{ userSelect: "none", lineHeight: "1.1" }}  title="Double tap to jump page" onDoubleClick={() => {
+                        this.setState({
+                            isEditabele: !this.state.isEditabele
+                        })
+                    }} >
+                        {1} of {100}
+                        <br/>
+                        <small>double tap to edit</small>
+                    </p>
+                )}
+                </div>
+                
+                <button className="btn btn-warning ml-auto">Next</button>
             </div>
         )
     }
