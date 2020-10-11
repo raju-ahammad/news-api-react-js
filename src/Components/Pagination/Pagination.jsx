1d11ec98cf29780d61516619fcb8786eb0ba69ab
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-11 23:57:38 +0600

    create pagination button component

--- src/Components/Pagination/Pagination.jsx
+++ src/Components/Pagination/Pagination.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
 
 class Pagination extends Component {
+    state = {
+        isEditabele: false
+    }
     render() {
         return (
-            <div>
-                
+            <div className="d-flex align-items-center my-5">
+                <button className="btn btn-warning">Previous</button>
             </div>
         )
     }
