a504d346710df15c469b60186e92831edf8f1721
raju-ahammad <web.rajuahammad@gmail.com>
2020-10-13 17:05:39 +0600

    create category

--- src/App.js
+++ src/App.js
@@ -7,54 +7,63 @@ import { NewsList } from './Components/NewsList/NewsList';
 import Loading from './Components/Pagination/Loading';
 import Pagination from './Components/Pagination/Pagination';
 
-const fakeNews = [
-  {
-    title: "Title",
-    content: "Con",
-    url: "htp",
-    urlToImage: "ht",
-    publishedAt: "ttime",
-    source: {
-      name: "CNN"
-    }
-  },
-  {
-    title: "Title",
-    content: "Con",
-    url: "htp",
-    urlToImage: "ht",
-    publishedAt: "ttime",
-    source: {
-      name: "CNN"
-    }
-  }
-]
-
 
 class App extends Component {
 
+  state = {
+    news: [],
+    category: newsCategory.technology
+  }
+
+  changeCategory = (category) => {
+    console.log(category);
+    this.setState({
+      category
+    })
+  }
+
   componentDidMount(){
-    const url = `${process.env.REACT_APP_NEWS_URL}?apiKey=${process.env.REACT_APP_API_KEY}&category=technology`;
+    const url = `${process.env.REACT_APP_NEWS_URL}?apiKey=${process.env.REACT_APP_API_KEY}&category=${this.state.category}&pageSize=5`;
     axios
     .get(url)
     .then((response)=>{
       console.log(response);
+      this.setState({
+        news: response.data.articles
+      })
     })
     .catch((e)=> {
       console.log(e);
     })
+  }
+
+  componentDidUpdate(prevProps, prevState){
+    if (prevState.category !== this.state.category) {
+      const url = `${process.env.REACT_APP_NEWS_URL}?apiKey=${process.env.REACT_APP_API_KEY}&category=${this.state.category}&pageSize=5`;
+    axios
+    .get(url)
+    .then((response)=>{
+      console.log(response);
+      this.setState({
+        news: response.data.articles
+      })
+    })
+    .catch((e)=> {
+      console.log(e);
+    })
+    }
   }
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-sm-6 offset-md-3">
-            <Header category = {newsCategory.technology}/>
+            <Header category = {this.state.category} changeCategory={this.changeCategory}/>
             <div className="d-flex">
               <p className="text-black-50">about {0} result found</p>
               <p className="text-black-50 ml-auto">{1} page of {100} </p>
             </div>
-            <NewsList news={ fakeNews }/>
+            <NewsList news={ this.state.news }/>
             <Pagination/>
             <Loading/>
           </div>
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -16,7 +16,7 @@ class Header extends Component {
 
     }
     render() {
-        const {category} = this.props
+        const {category, changeCategory} = this.props
         return (
             <div className="my-4">
                 <h1 className="mb-4 h3">News Headline</h1>
@@ -25,11 +25,11 @@ class Header extends Component {
                     { newsCategory && Object.keys(newsCategory).map(item=> {
                         if(category === newsCategory[item]){
                             return (
-                                <button className="btn btn-sm btn-warning mr-2 mb-2"> {  `#${newsCategory[item]}`} </button>
+                                <button onClick={()=> changeCategory(newsCategory[item])} className="btn btn-sm btn-warning mr-2 mb-2"> {  `#${newsCategory[item]}`} </button>
                             );        
                         }
                         return (
-                            <button className="btn btn-sm btn-light mr-2 mb-2"> {`#${newsCategory[item]}`} </button>
+                            <button onClick={()=> changeCategory(newsCategory[item])} className="btn btn-sm btn-light mr-2 mb-2"> {`#${newsCategory[item]}`} </button>
                         )
                     }) }
                 </div>
